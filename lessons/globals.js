06d0496eb2c193b1a6e06e5497151427b870c3c5
Justin Zimmerman <zimmy130@gmail.com>
2018-10-20 08:20:46 -0400

    add global setup for test and expect

---
+++ lessons/globals.js
@@ -0,0 +1,14 @@
+// node --require ./setup-globals.js lessons/globals.js
+const {sumAsync, subtractAsync} = require('../math');
+
+test('sum adds numbers asynchronously', async () => {
+  const result = await sumAsync(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test('subtract subtracts numbers asynchronously', async () => {
+  const result = await subtractAsync(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
